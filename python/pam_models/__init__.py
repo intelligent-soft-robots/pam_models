2694668c634966bc35481a69fdc6a9f4c08ba7f7
Vincent Berenz <vberenz@tuebingen.mpg.de>
2020-11-03 16:28:41 +0100

    set config folder to /opt/mpi-is

--- python/pam_models/__init__.py
+++ python/pam_models/__init__.py
@@ -1,14 +1,22 @@
 import os
 
+# config files are expected to be in /opt/mpi-is/
+_config_folder = "/opt/mpi-is"
+
 # return abs path to src/pam_models/config
 def get_config_folder():
-    from catkin_pkg import workspaces
-    packages = workspaces.get_spaces()
-    package_path = [ p for p in packages
-                     if p.endswith("pam_models") ][0]
-    return os.path.join(package_path,
-                        "config")
-                   
+    #
+    # deprecated: in source config
+    # folder was used
+    #
+    #from catkin_pkg import workspaces
+    #packages = workspaces.get_spaces()
+    #package_path = [ p for p in packages
+    #                 if p.endswith("pam_models") ][0]
+    #return os.path.join(package_path,
+    #                    "config")
+    global _config_folder
+    return _config_folder
 
 def get_config_path(config):
     config_folder = get_config_folder()
