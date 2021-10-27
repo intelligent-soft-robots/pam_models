280d7e09a8145c4649d4033d4faa00d1caff955e
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-10-27 17:33:55 +0200

    Reformat using black

--- python/pam_models/__init__.py
+++ python/pam_models/__init__.py
@@ -9,21 +9,23 @@ def get_config_folder():
     # deprecated: in source config
     # folder was used
     #
-    #from catkin_pkg import workspaces
-    #packages = workspaces.get_spaces()
-    #package_path = [ p for p in packages
+    # from catkin_pkg import workspaces
+    # packages = workspaces.get_spaces()
+    # package_path = [ p for p in packages
     #                 if p.endswith("pam_models") ][0]
-    #return os.path.join(package_path,
+    # return os.path.join(package_path,
     #                    "config")
     global _config_folder
     return _config_folder
 
+
 def get_config_path(config):
     config_folder = get_config_folder()
-    config_file = config_folder+os.sep+config+".json"
+    config_file = config_folder + os.sep + config + ".json"
     if not os.path.isfile(config_file):
-        raise FileNotFoundError("failed to find pam_models config file "+config_file)
+        raise FileNotFoundError("failed to find pam_models config file " + config_file)
     return config_file
+
 
 def get_default_config_path():
     return get_config_path("hill")
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['pam_models'],
-    package_dir={'': 'python'},
+    packages=["pam_models"],
+    package_dir={"": "python"},
 )
 
 setup(**setup_args)
